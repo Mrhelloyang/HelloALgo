4beaf2a82acfebacd947fd9bc1b309c4e55511d5
Mrhelloyang <2131424889@qq.com>
2024-03-18 16:57:15 +0800

    helloalgo firstcommit

---
+++ HelloAlgo/001_扩容数组.cpp
@@ -0,0 +1,25 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+using namespace std;
+int* extend(int* nums, int size, int enlarge)
+{
+	int* res = NULL;
+	res = new int[size + enlarge];
+	for (int i = 0; i < size; i++)
+	{
+		res[i] = nums[i];
+	}
+	delete[]nums;
+	return res;
+}
+int main()
+{
+	int* nums = new int[6] { 1, 2, 3, 4, 5, 6 };
+	int enlarge = 4;
+	nums = extend(nums, sizeof(nums), enlarge);
+	for (int i = 0; i < 6; i++)
+	{
+		cout << nums[i] << " ";
+	}
+	return 0;
+}
---
+++ HelloAlgo/002_链表.cpp
@@ -0,0 +1,49 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+using namespace std;
+
+struct ListNode {
+	int val;
+	ListNode* next;
+	ListNode(int x) :val(x), next(NULL) {}//构造函数
+};
+void test01()
+{
+	ListNode* n0 = new ListNode(1);
+	ListNode* n1 = new ListNode(2);
+	ListNode* n2 = new ListNode(3);
+	ListNode* n3 = new ListNode(4);
+	ListNode* n4 = new ListNode(5);
+	ListNode* n5 = new ListNode(6);
+	n0->next = n1;
+	n1->next = n2;
+	n2->next = n3;
+	n3->next = n4;
+	n4->next = n5;
+}
+//在节点n0后插入p节点
+void insert(ListNode* n0, ListNode* p)
+{
+	p = n0->next;
+	n0->next = p;
+}
+//删除n0后面的节点
+void remove(ListNode* n0)
+{
+	if (n0->next == NULL)
+	{
+		return;
+	}
+	ListNode* p;
+	p = n0->next;
+	n0->next = p->next;
+	delete p;
+
+}
+int main()
+{
+
+	test01();
+
+	return 0;
+}
